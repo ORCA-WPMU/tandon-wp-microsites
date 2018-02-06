8d5e47f32df1173808006b0ef8b01e75aee0658a
jamesgillispie <james.gillispie@gmail.com>
2018-02-06 15:49:23 -0500

    Increase posts output.

--- wp-content/themes/tandon/archive-people.php
+++ wp-content/themes/tandon/archive-people.php
@@ -27,7 +27,7 @@ if (!isset($paged) || !$paged){
 
 $journals = array(
     'post_type' => 'people',
-    'posts_per_page' => 2,
+    'posts_per_page' => 6,
     'paged' => $paged,
     'orderby' => array(
         'date' => 'DESC'
