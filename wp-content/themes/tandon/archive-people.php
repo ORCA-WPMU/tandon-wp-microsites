24e5e3b1f5b0347476b238d9d73e6a301f9548ad
jamesgillispie <james.gillispie@gmail.com>
2018-02-16 13:30:39 -0500

    Remove terms.

--- wp-content/themes/tandon/archive-people.php
+++ wp-content/themes/tandon/archive-people.php
@@ -13,7 +13,7 @@ $context['title_post_type'] = get_post_type();
 
 $context['slug'] = get_queried_object();
 
-$context['taxterms'] = Timber::get_terms('roles');
+//$context['taxterms'] = Timber::get_terms('roles');
 
 $context['title_term'] = get_term_by( 'slug', get_query_var('term'), get_query_var('taxonomy') );
 
